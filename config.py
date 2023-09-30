85566f0e97dd426a6660141b01ce3ddb4827a5e8
Sangram <136038979+OfficialSangram@users.noreply.github.com>
2023-09-30 18:46:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
-    START_PIC = "" 
-    CHAT = ""    
+    API_ID = 21571508
+    API_HASH = "d2f1d8d4a3626a0dc93bb46c1f827a88"
+    TOKEN = "6096057691:AAE4s-Cw7ZUfMd13lmojcPYniDbkzr2YN08"  
+    START_PIC = "https://telegra.ph/file/9a00b553b8cd8279ca026.jpg" 
+    CHAT = "https://t.me/Red_Wine_Op"    
