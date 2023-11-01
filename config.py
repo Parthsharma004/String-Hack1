2a4306564dc288250627538e58c8d84abbab8ec9
Parthsharma004 <134805985+Parthsharma004@users.noreply.github.com>
2023-11-01 10:57:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ class Config:
     API_HASH = "59cdcd1a9b95c004e7d45f4759e528a4"
     TOKEN = "6679179877:AAHxYcZpGfZOQV0MM57w2_yAUc6VOGKovkY"  
     START_PIC = "https://telegra.ph/file/9a00b553b8cd8279ca026.jpg" 
-    CHAT = "https://t.me/Red_Wine_Op"    
+    CHAT = "https://t.me/Diloo_Ki_Dhadkan"    
